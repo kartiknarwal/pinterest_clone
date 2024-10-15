4562c37ccdd5f53ff1c4281a727ba125ae2fa541
kartiknarwal <kartiknarwal38@gmail.com>
2024-10-15 08:47:46 +0530

    added first commit to pinterest clone repository

---
+++ backend/routes/pinRoutes.js
@@ -0,0 +1,25 @@
+import express from "express";
+import { isAuth } from "../middlewares/isAuth.js";
+import uploadFile from "../middlewares/multer.js";
+import {
+  commentOnPin,
+  createPin,
+  deleteComment,
+  deletePin,
+  getallPins,
+  getSinglePin,
+  updatePin,
+  
+} from "../controllers/pinControllers.js";
+
+const router = express.Router();
+
+router.post("/new", isAuth, uploadFile, createPin);
+router.get("/all",isAuth,getallPins);
+router.get("/:id",isAuth,getSinglePin);
+router.put("/:id",isAuth,updatePin);
+router.delete("/:id",isAuth,deletePin);
+router.post("/comment/:id",isAuth,commentOnPin);
+router.delete("/comment/:id",isAuth,deleteComment);
+
+export default router;
